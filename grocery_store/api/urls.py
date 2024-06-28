2fbb461be9cb16b18844e9739eba728d7eaddf54
Константин Стеблев <konstantin.sks85@gmail.com>
2024-06-28 16:53:32 +0200

    views

--- grocery_store/api/urls.py
+++ grocery_store/api/urls.py
@@ -2,18 +2,18 @@ from django.urls import include, path
 
 from rest_framework.routers import DefaultRouter
 
-from .views import (CustomUserViewSet, TagViewSet,
-                    IngredientViewSet, RecipeViewSet)
+from .views import (ProductViewSet, CategoryViewSet,
+                    ShoppingCartViewSet)
 
 app_name = 'api'
 
 
 router_v1 = DefaultRouter()
 
-router_v1.register(r'users', CustomUserViewSet, basename='users')
-router_v1.register(r'tags', TagViewSet, basename='tags')
-router_v1.register(r'recipes', RecipeViewSet, basename='recipes')
-router_v1.register(r'ingredients', IngredientViewSet, basename='ingredients')
+router_v1.register(r'products', ProductViewSet, basename='products')
+router_v1.register(r'categories', CategoryViewSet, basename='categories')
+router_v1.register(r'shopping-cart', ShoppingCartViewSet,
+                   basename='shopping-cart')
 
 urlpatterns = [
     path('', include(router_v1.urls)),
